568b2dde756e40fdf90756d61e8aaf0ecbf2fab2
Malleus15 <48280928+Malleus15@users.noreply.github.com>
2021-09-16 15:15:48 +0200

    Fixed system infeasibility

--- game.py
+++ game.py
@@ -88,14 +88,16 @@ class Game:
 
     def calculate_core(self, infos_all_coal_one_config):
         A_eq = [[1, 1, 1]]
-        b_eq = infos_all_coal_one_config[-1]["coalitional_payoff"]
+        b_eq = -infos_all_coal_one_config[-1]["coalitional_payoff"]
         A = [[-1, 0, 0], [0, -1, 0], [0, 0, -1], [-1, -1, 0], [-1, 0, -1], [0, -1, -1]]
         b = []
         for info in infos_all_coal_one_config[:-1]:
-            b.append(-info["coalitional_payoff"])
+            b.append(info["coalitional_payoff"])
         coefficients_min_y = [0] * (len(A[0]))
         res = linprog(coefficients_min_y, A_eq=A_eq, b_eq=b_eq, A_ub=A, b_ub=b)
+        print(A_eq, b_eq, A, b)
         print("AAA", res)
+        print(infos_all_coal_one_config)
 
     def is_convex(self, coalitions_infos):
         return cp.is_convex(coalitions_infos)
